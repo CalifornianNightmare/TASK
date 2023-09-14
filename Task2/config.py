b30a2a0cfc9dac75cc680b63184998f7ae988345
EgorZhizhlo <96652246+EgorZhizhlo@users.noreply.github.com>
2023-09-15 00:36:50 +0300

    Update config.py

--- Task2/config.py
+++ Task2/config.py
@@ -1,5 +1,9 @@
+from sqlalchemy import create_engine
+
 host = 'localhost'
 username = 'root'
 password = 'NeforMAL_768'
 db_name = 'testdb13'
 excel_filename = 'test.xlsx'
+engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}/{db_name}")
+excel_filename2 = 'test2.xlsx'
